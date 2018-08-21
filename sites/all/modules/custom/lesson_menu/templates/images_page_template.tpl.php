dbf117e51c3bd0b93432f077fbacdba1ff0e481b
Dzmitry Kruchko <dzmitry_kruchko@epam.com>
2018-08-21 10:35:05 +0300

    first hometask

--- sites/all/modules/custom/lesson_menu/templates/images_page_template.tpl.php
+++ sites/all/modules/custom/lesson_menu/templates/images_page_template.tpl.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 /**
@@ -20,3 +21,27 @@ print $list;
 foreach ($generated_images as $src) {
   print theme('image', $variables = ['path' => $src]);
 }
+=======
+<?php
+
+/**
+ * @file
+ * Displays a list of images and possible image styles.
+ *
+ * Available variables:
+ * - $original_image: contains HTML for an original (sample) image.
+ * - $list: contains a list of possible image styles.
+ * - $generated_images: array that contains generated miniatures, array keys are image styles names, array values are links to the images.
+ *
+ * @see lesson_menu_render_image().
+ *
+ */
+
+print $original_image;
+
+print $list;
+
+foreach ($generated_images as $src) {
+  print theme('image', $variables = ['path' => $src]);
+}
+>>>>>>> 986ad87... first hometask
